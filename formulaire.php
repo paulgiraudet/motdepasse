231fbf14cc9e7ddaed1c7ae213b1fb2b0e763a90
Tictac <paul.giraudet@gmail.com>
2018-09-06 15:33:59 +0200

    matching password display result, else error

--- formulaire.php
+++ formulaire.php
@@ -18,6 +18,12 @@
 
 <body>
 
+  <form action="secret.php" method="post">
+    <label for="password">Mot de Passe</label>
+    <input type='text' name="motdepasse" id="password">
+    <input type="submit" value="Se connecter">
+  </form>
+
 
   <script src="js/vendor/modernizr-3.6.0.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
---
+++ secret.php
@@ -0,0 +1,9 @@
+<?php
+if ($_POST['motdepasse']=='kangourou') {
+  echo 'Les codes d"acces de la NASA sont : une_fougère / un_eucalyptus';
+}
+else{
+  echo "intru détecté";
+}
+
+?>
